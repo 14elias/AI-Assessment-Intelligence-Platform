c571b92c95d4f5e796b90d86542d499c9835df5a
14elias <ellamebrahtom1995@gmail.com>
2025-12-22 02:08:05 +0300

    db setup

---
+++ db/base_class.py
@@ -0,0 +1,9 @@
+
+from db.base import Base
+from models.course_model import Course
+from models.deps_model import Department
+from models.objective_model import Objective
+from models.semister_model import Semester
+from models.topic_model import Topic
+from models.course_offering_model import CourseOffering
+from models.academic_year_model import AcademicYear
